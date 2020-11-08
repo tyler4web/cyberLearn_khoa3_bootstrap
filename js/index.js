9183e7f9c1c88eadc3a8f15f0fd2580543d363d2
Bui Quoc Trieu <buiquoctrieu0301@gmail.com>
2020-11-08 16:21:12 +1100

    initial commit (finish header and navbar, working on carousel)

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// <!-- Call carousel -->
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
